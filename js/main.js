e0dfb5fba40ab0f192eb5a7b8ddf5f0700ef4725
maucheja <jmaucher05@icloud.com>
2022-05-30 09:55:46 +0200

    Computer fertig programmiert

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ let punkte1 = 0
 let punkte2 = 0
 let AnzahlWuerfe1 = 0
 let AnzahlWuerfe2 = 0
+let AnzahlWuerfe3 = 0
 let wurf
 let wurf1, wurf2
-
+let durchschnitt
 
 function wuerfeln1() {
     wurf1 = Math.round(Math.random() * 5) + 1
@@ -50,13 +51,14 @@ function wuerfeln2() {
         document.getElementById("btnPlaceholder2").disabled = true
         auswerten()
     }
-    if (AnzahlWuerfe1 === AnzahlWuerfe2){
+    if (AnzahlWuerfe1 === AnzahlWuerfe2 || AnzahlWuerfe3 === AnzahlWuerfe2){
         document.getElementById("btnPlaceholder2").disabled = true
         auswerten()
     }
 }
 function ende() {
     document.getElementById("btnPlaceholder1").disabled = true
+    document.getElementById("btnPlaceholder6").disabled = true
 
 }
 function auswerten() {
@@ -75,7 +77,39 @@ function neu() {
 }
 
 function computer() {
-    while (i < )
-
-    
+    let weiterspielen = true
+    document.getElementById("btnPlaceholder1").disabled = true
+    do {
+        wurf1 = Math.round(Math.random() * 5) + 1
+        wurf2 = Math.round(Math.random() * 5) + 1
+        wurf = wurf1 + wurf2
+        AnzahlWuerfe3 = AnzahlWuerfe3 + 1
+        console.log(wurf)
+        console.log(AnzahlWuerfe3)
+        if (7 > wurf || wurf > 7) {
+            punkte1 = punkte1 + wurf
+            document.getElementById("Würfel1").innerHTML = "Würfel 1:  " + wurf1
+            document.getElementById("Würfel2").innerHTML = "Würfel 2:  " + wurf2
+            document.getElementById("Summe1").innerHTML = "Summe Spieler 1:  " + punkte1
+            document.getElementById("Anzahl1").innerHTML = "Anzahl Würfe Spieler 1:  " + AnzahlWuerfe3
+            durchschnitt = punkte1 / AnzahlWuerfe3
+            document.getElementById("btnPlaceholder6").disabled = false
+        } else {
+            punkte1 = punkte1 + wurf - 14
+            document.getElementById("Würfel1").innerHTML = "Würfel 1:" + wurf1
+            document.getElementById("Würfel2").innerHTML = "Würfel 2:" + wurf2
+            document.getElementById("Summe1").innerHTML = "Summe Spieler 1:" + punkte1
+            document.getElementById("Anzahl1").innerHTML = "Anzahl Würfe Spieler 1:  " + AnzahlWuerfe3
+            document.getElementById("btnPlaceholder6").disabled = true
+            weiterspielen = false
+        }
+        if (durchschnitt >= 8) {
+            weiterspielen = false
+            document.getElementById("btnPlaceholder6").disabled = true
+        }
+    } while (weiterspielen)
+}
+function beenden() {
+    document.getElementById("btnPlaceholder2").disabled = true
+    auswerten()
 }
